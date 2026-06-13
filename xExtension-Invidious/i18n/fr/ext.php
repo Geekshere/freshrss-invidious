20024015cae497237affc2cd1669de19a68001b2
Ethan Gotz <159620941+Geekshere@users.noreply.github.com>
2026-06-13 16:53:52 +0400

    Update ext.php

--- xExtension-Invidious/i18n/fr/ext.php
+++ xExtension-Invidious/i18n/fr/ext.php
@@ -1,13 +1,14 @@
 <?php
-return 
-  array(	
+
+return array(
 	'in_videos' => array(
-		'height' => 'Hauteur du lecteur',
-		'width' => 'Largeur du lecteur',
-		'updates' => 'Vous pouvez trouver la dernière mise à jour de l’extension sur ',
-        'show_content' => 'Ajouter également le contenu du flux',
-        'instance' => 'Instance invidious',
-        'youtube_link_text' => 'Regarder sur YouTube',
-        'replace_global' => 'Remplacer les intégrations Youtube dans tous les flux',
-  ),
+		'height'               => 'Hauteur du lecteur',
+		'width'                => 'Largeur du lecteur',
+		'updates'              => 'Basé sur les forks amont :',
+		'show_content'         => 'Afficher le lien « Regarder sur YouTube » sous chaque vidéo',
+		'instance'             => 'Instance Invidious',
+		'instance_placeholder' => 'nom d\'hôte uniquement, ex. inv.exemple.com',
+		'youtube_link_text'    => 'Regarder sur YouTube',
+		'replace_global'       => 'Remplacer les intégrations YouTube dans tous les flux (pas seulement les flux de chaînes YouTube)',
+	),
 );
