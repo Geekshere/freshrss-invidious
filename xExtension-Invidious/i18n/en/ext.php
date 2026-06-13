e016eb41cc2c9b1a0f10d72c5bbb59bc083404ea
Ethan Gotz <159620941+Geekshere@users.noreply.github.com>
2026-06-13 16:52:54 +0400

    Update localization strings in ext.php
    Updated text for the 'updates' and 'show_content' keys, added 'instance_placeholder' key, and modified the description for 'replace_global'.

--- xExtension-Invidious/i18n/en/ext.php
+++ xExtension-Invidious/i18n/en/ext.php
@@ -2,12 +2,13 @@
 
 return array(
 	'in_videos' => array(
-		'height' => 'Player height',
-        'width' => 'Player width',
-        'updates' => 'You can find the latest extension version at',
-        'show_content' => 'Additionally display a YouTube link',
-        'instance' => 'Invidious instance',
-        'youtube_link_text' => 'Watch on YouTube',
-        'replace_global' => 'Replace Youtube embeds in all feeds',
+		'height'               => 'Player height',
+		'width'                => 'Player width',
+		'updates'              => 'Based on upstream forks:',
+		'show_content'         => 'Show "Watch on YouTube" link below each video',
+		'instance'             => 'Invidious instance',
+		'instance_placeholder' => 'hostname only, e.g. inv.example.com',
+		'youtube_link_text'    => 'Watch on YouTube',
+		'replace_global'       => 'Replace YouTube embeds in all feeds (not just YouTube channel feeds)',
 	),
 );
